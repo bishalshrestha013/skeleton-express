daf96cc7e1f76c22464aad6fc0adc45c5ada6756
Bishal <mr.shrestha713@gmail.com>
2026-03-31 07:40:53 +0545

    feat: update mongodb setup

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,61 @@
-import mongoose from "mongoose"
-import { db } from "../config"
+import mongoose, { mongo } from "mongoose";
+import Logger from "../core/Logger";
+import { db } from "../config";
+import { run } from "node:test";
 
-const dbURI = `mongodb://${db.user}:${db.password}@${db.host}:${db.port}/${db.name}?authSource=admin`
+const dbURI = `mongodb://${db.user}:${encodeURIComponent(db.password)}@${db.host}:${db.port}/${db.name}?authSource=admin`;
+
+const options = {
+  autoIndex: true,
+  minPoolSize: db.minPoolSize,
+  maxPoolSize: db.maxPoolSize,
+  connectTimeoutMS: 10000,
+  socketTimeoutMS: 45000,
+};
+
+Logger.debug(dbURI);
+
+function setRunValidators() {
+  return { runValidators: true };
+}
+
+mongoose.set("strictQuery", true);
 
 mongoose
-  .connect(dbURI)
-  .then(() => "MongoDB Connected")
-  .catch(err => console.log(err))
+  .plugin((schema: any) => {
+    schema.pre("findOneAndUpdate", setRunValidators);
+    schema.pre("updateMany", setRunValidators);
+    schema.pre("updateOne", setRunValidators);
+    schema.pre("update", setRunValidators);
+  })
+  .connect(dbURI, options)
+  .then(() => {
+    Logger.info("Mongoose connection done");
+  })
+  .catch((err) => {
+    Logger.info("Mongoose connection error");
+    Logger.error(err);
+  });
+
+// CONNECTION EVENTS
+// when successfully connected
+mongoose.connection.on("connected", () => {
+  Logger.debug("Mongoose default connection open to " + dbURI);
+});
+
+// If the connection throws an error
+mongoose.connection.on("error", (err) => {
+  Logger.error("Mongoose default connection error: " + err);
+});
+
+// if the Node process ends, close the Mongoose connection
+process.on("SIGINT", () => {
+  mongoose.connection.close().finally(() => {
+    Logger.info(
+      "Mongoose default connection disconnected through app termination",
+    );
+    process.exit(0);
+  });
+});
+
+export const connection = mongoose.connection;
